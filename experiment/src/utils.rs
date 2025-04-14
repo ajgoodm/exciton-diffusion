454ecff53a1103007ac47443f0ddb04765591b5e
Aaron Aster <aarongoodm@gmail.com>
2025-04-14 10:44:10 -0400

    feat: expose simulation via CLI

--- experiment/src/utils.rs
+++ experiment/src/utils.rs
@@ -1,11 +1,19 @@
-use std::{f64, sync::LazyLock};
+use std::{path::PathBuf, sync::LazyLock};
 
 use rand::rngs::ThreadRng;
 use rand_distr::{Distribution, Uniform};
+use serde::de::DeserializeOwned;
+use serde_json::from_str;
 
 static UNIFORM_ZERO_TO_PI: LazyLock<Uniform<f64>> =
-    LazyLock::new(|| Uniform::new(0.0, f64::consts::PI).unwrap());
+    LazyLock::new(|| Uniform::new(0.0, std::f64::consts::PI).unwrap());
 
 pub fn random_uniform_zero_to_pi(rng: &mut ThreadRng) -> f64 {
     UNIFORM_ZERO_TO_PI.sample(rng)
+}
+
+pub fn read_json_file<T: DeserializeOwned>(path: PathBuf) -> Result<T, serde_json::Error> {
+    from_str(
+        &std::fs::read_to_string(path.clone()).expect(&format!("failed to read file {:?}", path)),
+    )
 }
